eb05a1262b2c6546ef714c77321e4fc4553faa3d
429AU <1017439429@qq.com>
2020-11-20 13:12:21 +0800

    branch

--- 新建文本文档.c
+++ 新建文本文档.c
@@ -1,10 +1,4 @@
 int main(){
-	printf("Hello Git!");
-	printf("Commit 1");
-	printf("Commit 2");
-	printf("Commit 3");
-
-	printf("branch1");
-	printf("Commit m4")
+	printf("Hello Branch!");
 	return 0;
 }
